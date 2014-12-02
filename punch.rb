35bae410466c78ba8a72e9305a003b5300339951
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2014-12-02 23:00:27 +0100

    Initial commit

---
+++ punch.rb
@@ -0,0 +1,288 @@
+#!/usr/bin/env ruby
+
+class Time
+  def short_year
+    strftime('%y').to_i
+  end
+end
+
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def green
+    colorize 32
+  end
+
+  def blue
+    colorize 34
+  end
+
+  def pink
+    colorize 35
+  end
+end
+
+class BRFParser
+  TOTAL = /Total:.+$/
+  ParserError = Class.new(StandardError)
+  def parse(str)
+    lines = str.split("\n").map(&:strip).reject &:empty?
+    month = Month.new lines.shift
+    lines.pop if lines.last =~ TOTAL
+    month.days = lines.map do |l|
+      l.sub! TOTAL, ''
+      day_ary    = l.split
+      day        = Day.new(day_ary.shift)
+      day.blocks = day_ary.map { |block_str| Block.new block_str, day }
+      day
+    end
+    month
+  rescue StandardError => e
+    raise ParserError.new("Couldn't parse string: " + e.message)
+  end
+end
+
+module Totals
+  def total
+    children.inject(0) { |sum, c| sum += c.total }
+  end
+
+  def total_str
+    hours   = total / 3600
+    rest    = total - (hours * 3600)
+    minutes = rest / 60
+    "#{pad hours}:#{pad minutes}"
+  end
+
+  private
+
+  def pad(number)
+    number.to_i.to_s.rjust 2, '0'
+  end
+end
+
+class Block
+  include Comparable
+  attr_accessor :start, :finish
+
+  def initialize(str, day)
+    start_str, finish_str = str.split '-'
+
+    start_ary  = start_str.split(':')
+    finish_ary = finish_str.split(':')
+
+    @start  = Time.new(day.long_year, day.month, day.day, *start_ary)
+    @finish = Time.new(day.long_year, day.month, day.day, *finish_ary)
+  end
+
+  def to_s
+    "#{format start}-#{format finish}"
+  end
+
+  def total
+    (finish - start).to_i
+  end
+
+  def <=>(other)
+    start <=> other.start
+  end
+
+  def include?(time)
+    (start <= time) && (finish >= time)
+  end
+
+  private
+
+  def format(time)
+    time.strftime "%H:%M"
+  end
+end
+
+class Day
+  include Totals
+  include Comparable
+
+  attr_accessor :day, :month, :year, :blocks
+
+  def initialize(date = '')
+    @day, @month, @year = date.split('.').map &:to_i
+  end
+
+  def date
+    "#{pad day}.#{pad month}.#{year}"
+  end
+
+  def to_s(color = false)
+    blocks.sort!
+    str = "#{date}   #{blocks.join('   ')}   Total: #{total_str}"
+    if color
+      return str.green if highlight?
+      return str.blue if today?
+    end
+    str
+  end
+
+  def blocks
+    @blocks ||= []
+  end
+
+  def add(*blocks)
+    highlight!
+    blocks.each do |block|
+
+      self.blocks.reject! { |b|
+        b.start > block.start && b.finish < block.finish }
+
+      if self.blocks.any? { |b|
+        b.start < block.start && b.finish > block.finish }
+        next
+      end
+
+      if (overlap = self.blocks.find { |b| b.include?(block.finish) })
+        overlap.start = block.start
+        next
+      end
+
+      if (overlap = self.blocks.find { |b| b.include?(block.start) })
+        overlap.finish = block.finish
+        next
+      end
+
+      self.blocks << block
+    end
+  end
+
+  def children
+    blocks
+  end
+
+  def long_year
+    year + 2000
+  end
+
+  def <=>(other)
+    if month == other.month
+      day <=> other.day
+    else
+      month <=> other.month
+    end
+  end
+
+  def at?(time)
+    (day == time.day) && (month == time.month) && (year == time.short_year)
+  end
+
+  def today?
+    at? Time.now
+  end
+
+  def set(time)
+    @day   = time.day
+    @month = time.month
+    @year  = time.short_year
+  end
+
+  def highlight!
+    @highlight = true
+  end
+
+  def highlight?
+    @highlight
+  end
+end
+
+class Month
+  include Totals
+
+  NAMES = {
+    1  => 'januar',
+    2  => 'februar',
+    3  => 'maerz',
+    4  => 'april',
+    5  => 'mai',
+    6  => 'juni',
+    7  => 'juli',
+    8  => 'august',
+    9  => 'september',
+    10 => 'oktober',
+    11 => 'november',
+    12 => 'dezember',
+  }
+
+  attr_accessor :name, :days
+
+  def self.name(month_nr)
+    NAMES[month_nr]
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s(color = false)
+    days.sort!
+    "#{name}\n\n#{days.map { |d|
+      d.to_s(color) }.join("\n")}\n\nTotal: #{total_str}"
+  end
+
+  def colored
+    to_s true
+  end
+
+  def children
+    days
+  end
+end
+
+if __FILE__ == $0
+  option = ARGV.first
+  this_folder = __FILE__[/\/.+\//]
+  hours_folder = this_folder + 'hours/'
+  if option == '-H' || option == '--hack'
+    `open #{__FILE__}`
+    exit
+  end
+  if option == '-h' || option == '--help'
+    puts File.read "#{this_folder}help.txt"
+    exit
+  end
+  now = Time.now
+  month_nr = now.month
+  month_nr -= 1 if now.day < 21
+  month_name = Month.name(month_nr)
+  year = now.year
+  filepath = "#{hours_folder}#{month_name}_#{year}.txt"
+  unless File.exists? filepath
+    File.open(filepath, "w") { |f| f.write "#{month_name.capitalize} #{year}" }
+  end
+  if option == '-e' || option == '--edit'
+    `open #{filepath}`
+    exit
+  end
+  File.open filepath, 'r+' do |file|
+    month = BRFParser.new.parse(file.read)
+    unless ARGV.empty?
+      if option == '-d' || option == '--day'
+        ARGV.shift
+        date = ARGV.shift
+        unless (day = month.days.find { |d| d.date == date })
+          day = Day.new date
+          month.days << day
+        end
+      else
+        unless (day = month.days.find { |d| d.at? now })
+          day = Day.new
+          day.set now
+          month.days << day
+        end
+      end
+      blocks = ARGV.map { |block_str| Block.new block_str, day }
+      day.add *blocks
+      file.seek 0, IO::SEEK_SET
+      file.write month
+    end
+    puts month.colored
+  end
+end
---
+++ punch_test.rb
@@ -0,0 +1,92 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative 'punch'
+
+class MonthTest < MiniTest::Unit::TestCase
+  def test_total_align
+    month = BRFParser.new.parse(<<-EOS)
+      November 2014
+
+      28.11.14   18:00-19:00   Total: 01:00
+      30.11.14   08:00-13:00   Total: 05:00
+
+      Total: 21:00
+    EOS
+
+    day1, day2 = month.days
+    day2.add Block.new('14-16', day2)
+
+    assert_equal "28.11.14   18:00-19:00                 Total: 01:00",
+      day1.to_s
+
+    assert_equal "30.11.14   08:00-13:00   14:00-16:00   Total: 05:00",
+      day2.to_s
+  end
+end
+
+class DayTest < MiniTest::Unit::TestCase
+  def test_initialize
+    day = Day.new '27.11.14'
+    assert_equal 27, day.day
+    assert_equal 11, day.month
+    assert_equal 14, day.year
+  end
+
+  def test_total
+    day = Day.new '08.04.91'
+    day.add Block.new("08:00-12:00", day)
+    day.add Block.new("13:00-18:00", day)
+    assert_equal 32400, day.total
+    assert_equal '09:00', day.total_str
+  end
+
+  def test_total_with_minutes
+    day = Day.new '08.04.91'
+    day.add Block.new("12:30-19:15", day)
+    assert_equal '06:45', day.total_str
+  end
+
+  def test_block_ordering
+    day = Day.new '26.03.89'
+    day.add Block.new("13:30-17:00", day)
+    day.add Block.new("06:00-11:45", day)
+    assert_equal "26.03.89   06:00-11:45   13:30-17:00   Total: 09:15", day.to_s
+  end
+
+  def test_at
+    now = Time.now
+    today = Day.new
+    today.set now
+    assert today.at?(now), 'today was not today'
+  end
+
+  def test_merge
+    day = Day.new '26.03.89'
+    day.add Block.new("13:15-17:00", day)
+    assert_equal '26.03.89   13:15-17:00   Total: 03:45', day.to_s
+    day.add Block.new("13:00-18:00", day)
+    assert_equal '26.03.89   13:00-18:00   Total: 05:00', day.to_s
+  end
+
+  def test_add_ignore
+    day = Day.new '12.04.95'
+    day.add Block.new("13:00-17:00", day)
+    day.add Block.new("14:00-16:00", day)
+    assert_equal '12.04.95   13:00-17:00   Total: 04:00', day.to_s
+  end
+
+  def test_start_merge
+    day = Day.new '12.04.95'
+    day.add Block.new("13:15-17:00", day)
+    day.add Block.new("13:00-17:00", day)
+    assert_equal '12.04.95   13:00-17:00   Total: 04:00', day.to_s
+  end
+
+  def test_end_merge
+    day = Day.new '12.04.95'
+    day.add Block.new("13:00-17:00", day)
+    day.add Block.new("16:00-18:00", day)
+    assert_equal '12.04.95   13:00-18:00   Total: 05:00', day.to_s
+  end
+end

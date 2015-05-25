429357e9acc88a1d313f9db7deb0e67e4c52ba9e
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2015-05-25 12:45:56 +0200

    Added a test for shadow --remove

--- test/remove_test.rb
+++ test/remove_test.rb
@@ -14,38 +14,51 @@ class RemoveTest < PunchTest
     brf_write %{
       Februar 2015
 
-      01.02.15   08:00-12:00   Total: 04:00
+      28.01.15   08:00-12:00   Total: 04:00
 
       Total: 04:00
     }
 
     punch '--remove 11:30-12'
-    assert_punched '01.02.15   08:00-11:30   Total: 03:30'
+    assert_punched '28.01.15   08:00-11:30   Total: 03:30'
   end
 
   def test_removing_at_start
     brf_write %{
       Februar 2015
 
-      01.02.15   08:00-12:00   Total: 04:00
+      28.01.15   08:00-12:00   Total: 04:00
 
       Total: 04:00
     }
 
     punch '--remove 8-11'
-    assert_punched '01.02.15   11:00-12:00   Total: 01:00'
+    assert_punched '28.01.15   11:00-12:00   Total: 01:00'
   end
 
   def test_splitting
     brf_write %{
       Februar 2015
 
-      01.02.15   08:00-12:00   Total: 04:00
+      28.01.15   08:00-12:00   Total: 04:00
 
       Total: 04:00
     }
 
     punch '--remove 9-10'
-    assert_punched '01.02.15   08:00-09:00 10:00-12:00   Total: 03:00'
+    assert_punched '28.01.15   08:00-09:00 10:00-12:00   Total: 03:00'
+  end
+
+  def test_shadowing
+    brf_write %{
+      Februar 2015
+
+      28.01.15   08:00-12:00   13:00-17:00   Total: 08:00
+
+      Total: 08:00
+    }
+
+    punch '--remove 7-12:30'
+    assert_punched '28.01.15   13:00-17:00   Total: 04:00'
   end
 end

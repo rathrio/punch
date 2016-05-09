985dd54a7678bd949a3b8fd1e4417edba9c95a7f
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2016-05-09 10:51:08 +0200

    Add time tests

---
+++ test/time_test.rb
@@ -0,0 +1,29 @@
+require_relative 'config'
+
+class TimeTest < MiniTest::Test
+  def test_short_year
+    assert_equal 16, Time.new(2016).short_year
+    assert_equal 99, Time.new(1999).short_year
+    assert_equal 2, Time.new(3002).short_year
+  end
+
+  def test_acts_like_a_block
+    t = Time.new
+    assert_equal t, t.start
+    assert_equal t, t.finish
+  end
+
+  def test_prev_day
+    t = Time.new(2016, 5, 12)
+    prev_day_t = Time.new(2016, 5, 11)
+
+    assert_equal prev_day_t, t.prev_day
+  end
+
+  def test_next_day
+    t = Time.new(2016, 5, 12)
+    next_day_t = Time.new(2016, 5, 13)
+
+    assert_equal next_day_t, t.next_day
+  end
+end
